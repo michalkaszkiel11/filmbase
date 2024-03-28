e0c6261bf8c6d8cc26dc7bac9120471b8725b811
Michal <149673103+michalkaszkiel11@users.noreply.github.com>
2024-03-28 10:46:33 +0100

    landing page update

--- filmbase/src/Main/LandingPage/LandingPage.jsx
+++ filmbase/src/Main/LandingPage/LandingPage.jsx
@@ -12,11 +12,11 @@ export const LandingPage = ({ handleFocus }) => {
                         Movies <span className="header-span">tonight</span>
                     </h1>
                     <p>
-                        Welcome to our website, where you can explore a vast
+                        Welcome to our website, where you can explore a huge
                         collection of movies! Discover detailed information
                         about each film, including the Title, Year of release,
                         Poster images, Runtime, IMDb rating, and User rating.
-                        Immerse yourself in the world of cinema by exploring
+                        Get ready to join the world of cinema by exploring
                         additional details such as Plot, Release date, Actors,
                         Director, and Genre. Enjoy the ultimate movie experience
                         with our comprehensive database at your fingertips.
