fa669c41655b604d7c7faabf1c1f4633fd8e5aa7
Michal <149673103+michalkaszkiel11@users.noreply.github.com>
2024-05-17 16:42:33 +0200

    +

--- filmbase/src/User/Collection/Film.jsx
+++ filmbase/src/User/Collection/Film.jsx
@@ -1,12 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { StarRating } from "../../StarRating";
 import { api } from "../../utils/apiInstance";
 import { Spinner } from "../../fillers/Spinner";
 import { deleteMovie } from "../../methods/deleteMovie";
-export const Film = ({ watch, loggedInUser, setIsWatchedUpadted }) => {
+// import { FilmDetails } from "./FilmDetails";
+
+export const Film = ({
+    watch,
+    loggedInUser,
+    setIsWatchedUpadted,
+    getMovie,
+    movieDetails,
+}) => {
     const email = loggedInUser ? loggedInUser.email : "";
     const [userRating, setUserRating] = useState(watch.userRating);
-
+    const [isDetailClicked, setIsDetailClicked] = useState(false);
+    const filmRef = useRef();
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (filmRef.current && !filmRef.current.contains(e.target)) {
+                setIsDetailClicked(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
     const updateUserRating = async () => {
         const payload = {
             email: email,
@@ -37,7 +56,9 @@ export const Film = ({ watch, loggedInUser, setIsWatchedUpadted }) => {
             setIsWatchedUpadted(false);
         }
     };
-
+    const isDetailStyles = {
+        width: "90%",
+    };
     useEffect(() => {
         updateUserRating();
     }, [userRating]);
@@ -45,37 +66,51 @@ export const Film = ({ watch, loggedInUser, setIsWatchedUpadted }) => {
     return (
         <>
             {watch ? (
-                <div className="film-box">
-                    <i
-                        className="fa fa-solid fa-xmark close-movie"
-                        onClick={() =>
-                            deleteMovie(email, watch, setIsWatchedUpadted)
-                        }
-                    ></i>
-                    <img src={watch.Poster} alt="poster" />
-                    <div className="film-box-text">
-                        <h3>{watch.Title}</h3>
-                        <p>{watch.Year}</p>
+                <div
+                    ref={filmRef}
+                    className="film-box-wrapper"
+                    style={isDetailClicked ? isDetailStyles : {}}
+                    onClick={(e) => {
+                        getMovie(watch);
+                        console.log("movieDetails", watch);
+                        setIsDetailClicked(true);
+                    }}
+                >
+                    <div className="film-box">
+                        <i
+                            className="fa fa-solid fa-xmark close-movie"
+                            onClick={() =>
+                                deleteMovie(email, watch, setIsWatchedUpadted)
+                            }
+                        ></i>
+                        <img src={watch.Poster} alt="poster" />
+                        <div className="film-box-text">
+                            <h3>{watch.Title}</h3>
+                            <p>{watch.Year}</p>
+                        </div>
+                        <div className="film-imdb-rating-box">
+                            <p>imdb rating: </p>
+                            <StarRating
+                                rating={watch.imdbRating}
+                                maxRating={10}
+                                disabled={true}
+                                size={18}
+                            />
+                        </div>
+                        <div className="film-imdb-rating-user">
+                            <p>your rating</p>
+                            <StarRating
+                                rating={watch.userRating}
+                                maxRating={10}
+                                disabled={false}
+                                size={18}
+                                setRating={setUserRating}
+                            />
+                        </div>
                     </div>
-                    <div className="film-imdb-rating-box">
-                        <p>imdb rating: </p>
-                        <StarRating
-                            rating={watch.imdbRating}
-                            maxRating={10}
-                            disabled={true}
-                            size={18}
-                        />
-                    </div>
-                    <div className="film-imdb-rating-user">
-                        <p>your rating</p>
-                        <StarRating
-                            rating={watch.userRating}
-                            maxRating={10}
-                            disabled={false}
-                            size={18}
-                            setRating={setUserRating}
-                        />
-                    </div>
+                    {isDetailClicked &&
+                        // <FilmDetails movieDetails={movieDetails} />
+                        movieDetails.Plot}
                 </div>
             ) : (
                 <Spinner />
--- filmbase/src/User/Collection/MovieList.jsx
+++ filmbase/src/User/Collection/MovieList.jsx
@@ -1,15 +1,29 @@
 import { Film } from "./Film";
+import { useState } from "react";
+
 export const MovieList = ({ watched, loggedInUser, setIsWatchedUpadted }) => {
+    const [movieDetails, setMovieDetails] = useState(null); // Use null for initial state
+    const getMovie = (watch) => {
+        const movie = watched.find((movie) => movie.Poster === watch.Poster);
+        if (movie) {
+            setMovieDetails(movie);
+        }
+    };
+
     return (
-        <ul className="movie-list-ul">
-            {watched.map((watch) => (
-                <Film
-                    key={watch._id}
-                    watch={watch}
-                    loggedInUser={loggedInUser}
-                    setIsWatchedUpadted={setIsWatchedUpadted}
-                />
-            ))}
-        </ul>
+        <div>
+            <ul className="movie-list-ul">
+                {watched.map((watch) => (
+                    <Film
+                        key={watch._id}
+                        watch={watch}
+                        loggedInUser={loggedInUser}
+                        setIsWatchedUpadted={setIsWatchedUpadted}
+                        getMovie={getMovie}
+                        movieDetails={movieDetails}
+                    />
+                ))}
+            </ul>
+        </div>
     );
 };
